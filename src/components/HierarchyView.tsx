26283778d127640201c745e4bed9feeb362d21f6
sni-oh <sh-ooyama@outlook.jp>
2025-12-16 20:40:23 +0900

    取得したデータを表示できる状態に

--- src/components/HierarchyView.tsx
+++ src/components/HierarchyView.tsx
@@ -1,18 +1,32 @@
-import React, { useEffect } from "react"
-import { useState } from "react";
-import type { FolderNode, IndexJson } from '../types/types';
+import type { FolderNode } from '../types/types';
 
 interface HierarchViewProps{
+  // この要素に関連づけられたノード
   node: FolderNode
+  // この要素を含むパス
+  path: string
 }
 
-const HierarchView: React.FC<HierarchViewProps> = ({node}) => {
-  
+const HierarchView: React.FC<HierarchViewProps> = ({node, path}) => {
+
+  node.childrenFolder.sort((a, b) => {
+    const numa = Number(a.folderName);
+    const numb = Number(b.folderName)
+    return numa - numb;
+  })
+
   return (
     <>
-      <p>{node.folderName}</p>
+      <p>{path}</p>
       {node.childrenFolder && node.childrenFolder.map((child) => {
-        return <HierarchView key={child.folderName} node={child}/>
+        return <HierarchView key={child.folderName} node={child} path={`${path}/${child.folderName}`}/>
+      })}
+      {node.files.map((imgName) => {
+        return (
+          <>
+            <img width='200px' src={`${path}/${imgName}`} loading="lazy"></img>
+          </>
+        )
       })}
     </>
   )
